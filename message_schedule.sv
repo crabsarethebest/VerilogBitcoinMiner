d01f3de6e89320ef63e704bb7459edbf31530994
Jonathan Anchell <jonanchell@gmail.com>
2024-04-10 14:10:11 -0700

    message schedule creation working

--- message_schedule.sv
+++ message_schedule.sv
@@ -20,18 +20,22 @@
 
 
 module message_schedule(
-    input [0:511] message_block,
-    output [0:511] message_schedule
+    input logic [0:511] message_block,
+    output logic [0:31] message_schedule[0:63]
     );
-
-initial begin  
-    logic [0:31] x;
-    logic [0:31] y;
-    x = 32'b00000000000000001111111111111111;
-    y = '0;
     
-    $display("x is %b", x);
-    y = sigma_functions::upper_sigma_one(x);
-    $display("y is %b", y);
-end
+
+    always_comb begin
+        for (int i = 0; i < 16; i++) begin
+            message_schedule[i] = message_block[i*32+ : 32];
+        end
+        
+        for (int j = 16; j < 64; j++) begin
+            logic [0:31] message;
+            message = sigma_functions::lower_sigma_one(message_schedule[j-2]) + message_schedule[j-7] + sigma_functions::lower_sigma_zero(message_schedule[j-15]) + message_schedule[j-16];
+            message_schedule[j] = message;
+            
+        end
+    end
+
 endmodule
